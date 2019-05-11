921707895d40313c26232997084e53fa2b3d6816
Benjamin <bene.a99@gmail.com>
2019-05-11 15:50:38 +0200

    added paddel movement

--- PongApp/Form1.Designer.cs
+++ PongApp/Form1.Designer.cs
@@ -29,12 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.paddle1 = new System.Windows.Forms.PictureBox();
+            this.paddle2 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            resources.ApplyResources(this.ball, "ball");
+            this.ball.Name = "ball";
+            this.ball.TabStop = false;
+            // 
+            // paddle1
+            // 
+            resources.ApplyResources(this.paddle1, "paddle1");
+            this.paddle1.Name = "paddle1";
+            this.paddle1.TabStop = false;
+            // 
+            // paddle2
+            // 
+            resources.ApplyResources(this.paddle2, "paddle2");
+            this.paddle2.Name = "paddle2";
+            this.paddle2.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.paddle2);
+            this.Controls.Add(this.paddle1);
+            this.Controls.Add(this.ball);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox paddle1;
+        private System.Windows.Forms.PictureBox paddle2;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- PongApp/Form1.cs
+++ PongApp/Form1.cs
@@ -12,9 +12,75 @@ namespace PongApp
 {
     public partial class Form1 : Form
     {
+
+        const int paddleSpeed = 2;
+
+        bool isUpPressed, isDownPressed, isWPressed, isSPressed = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(isUpPressed)
+            {
+                paddle1.Location = new Point(paddle1.Location.X, paddle1.Location.Y - paddleSpeed);
+            }
+
+            if (isDownPressed)
+            {
+                paddle1.Location = new Point(paddle1.Location.X, paddle1.Location.Y + paddleSpeed);
+            }
+
+            if (isWPressed)
+            {
+                paddle2.Location = new Point(paddle2.Location.X, paddle2.Location.Y - paddleSpeed);
+            }
+
+            if (isSPressed)
+            {
+                paddle2.Location = new Point(paddle2.Location.X, paddle2.Location.Y + paddleSpeed);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Up)
+            {
+                isUpPressed = true;
+            } else if (e.KeyCode == Keys.Down)
+            {
+                isDownPressed = true;
+            } else if (e.KeyCode == Keys.W)
+            {
+                isWPressed = true;
+            } else if (e.KeyCode == Keys.S)
+            {
+                isSPressed = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                isUpPressed = false;
+            } else if (e.KeyCode == Keys.Down)
+            {
+                isDownPressed = false;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                isWPressed = false;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                isSPressed = false;
+            }
         }
     }
 }
